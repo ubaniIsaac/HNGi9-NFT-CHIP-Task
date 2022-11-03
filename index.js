92f8e53c6c9bc0f6e292a4f022cb4b4c008767ad
Isaac Ubani <isaacchimdi@gmail.com>
2022-11-03 10:45:27 +0100

    HNGi9 NFT

---
+++ index.js
@@ -0,0 +1,105 @@
+const { parse } = require('csv-parse');
+const fs = require('fs');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const sha256 = require('sha256');
+const json2csv = require('json2csv');
+const dataArray = [];
+
+const csvWriter = createCsvWriter({
+    path: 'hashing.output.csv',
+    header: [{ id: 'Series_Number', title: 'Series_Number' },
+    { id: 'Filename', title: 'Filename' },
+    { id: 'Name', title: 'Name' },
+    { id: 'Description', title: 'Description' },
+    { id: 'Gender', title: 'Gender' },
+    { id: 'Attributes', title: 'Attributes' },
+    { id: 'UUID', title: 'UUID' },
+    { id: 'HASH', title: 'HASH' }],
+});
+
+fs.createReadStream("HNGi9 CSV FILE - Sheet1.csv")
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+        delimiter: ",",
+        from_line: 1
+    }))
+
+    .on("data", async (row) => {
+
+        const dataHash = sha256(JSON.stringify(row))
+        row.HASH = dataHash;
+        dataArray.push(row);
+        // console.log(dataArray)
+
+
+
+        const format = {
+            "format": "CHIP-0007",
+            "name": row.Name,
+            "description": row.Description,
+            "minting_tool": "SuperMinter/2.5.2",
+            "sensitive_content": false,
+            "series_number": row.Series_Number,
+            "series_total": 420,
+            "attributes": [
+                {
+                    "trait_type": "gender",
+                    "value": row.Gender
+                },
+                {
+                    "trait_type": "UUID",
+                    "value": row.UUID
+                },
+                {
+                    "trait_type": "attributes",
+                    "value": row.Attributes
+
+                }
+
+            ],
+            "collection": {
+                "name": row.Filename,
+                "id": "e43fcfe6-1d5c-4d6e-82da-5de3aa8b3b57",
+                "attributes": [
+                    {
+                        "type": "description",
+                        "value": row.Description
+                    }
+                ]
+            }
+        }
+        const dataString = JSON.stringify(format)
+
+        fs.writeFile(`CHIP-0007/nft${row.Filename}.json`, dataString,
+            {
+                encoding: "utf8",
+                flag: "w",
+                mode: 0o666
+            },
+            (err) => {
+                if (err)
+                    console.log(err);
+                else {
+                }
+            });
+
+        // fs.writeFileSync("hashing.output.csv", newCSV,
+        //     (err) => {
+        //         if (err)
+        //             console.log(err);
+        //         else {
+        //         }
+        //     });
+
+    })
+
+    .on('end', function () {
+        const newCSV = JSON.stringify(dataArray)
+
+        csvWriter.writeRecords(dataArray)       // returns a promise
+            .then(() => {
+                console.log('...Done');
+            });
+        // fs.writeFileSync("hashing.output.csv", newCSV);
+    });
